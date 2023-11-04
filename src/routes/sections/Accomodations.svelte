4f4d8c6d13a7b76d76ab4c6dfafc15b2d6778d3c
Tyler Patton <jamestylerpatton@gmail.com>
2023-11-03 19:17:51 -0700

    update hotel reservation link

--- src/routes/sections/Accomodations.svelte
+++ src/routes/sections/Accomodations.svelte
@@ -4,7 +4,11 @@
 >
 	<div class="text-center">
 		<h2 class="text-xs font-sans mb-10 uppercase">Accomodations</h2>
-		<h3 class="font-serif text-5xl mb-10">The Ace Hotel, Downtown</h3>
+		<h3 class="font-serif text-5xl mb-10">
+			<a href="https://acehotel.com/los-angeles/" target="_blank" rel="noopener noreferrer"
+				>The Ace Hotel</a
+			>, Downtown
+		</h3>
 		<p class="text-xs lg:text-base font-sans mb-2">
 			There is a block of rooms reserved at The Ace Hotel in Downtown LA.
 		</p>
@@ -12,7 +16,7 @@
 			If you'd like to join, let us know and we will reserve another for you!
 		</p>
 		<a
-			href="https://acehotel.com/los-angeles/"
+			href="https://reservations.acehotel.com/?adult=1&arrive=2024-02-29&chain=7231&child=0&currency=USD&depart=2024-03-02&group=GUPA0301&hotel=60514&level=hotel&locale=en-US&rooms=1"
 			target="_blank"
 			rel="noopener noreferrer"
 			class="inline-block uppercase bg-transparent focus:shadow-outline focus:outline-none text-text border-2 border-text py-5 px-10 lg:px-20 rounded cursor-pointer text-sm lg:text-base hover:bg-text hover:text-background transition-all"
