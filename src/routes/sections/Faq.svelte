10696136705d802a73c4af3c02b1faf2196a85ee
Tyler Patton <jamestylerpatton@gmail.com>
2023-12-03 13:01:33 -0800

    feat: update FAQs

--- src/routes/sections/Faq.svelte
+++ src/routes/sections/Faq.svelte
@@ -19,39 +19,24 @@
 				'If we do not receive an RSVP by the provided deadline, it will be marked as a "No." We will miss celebrating with you, but we need to provide a total guest count to the venue and vendors shortly after the deadline. Late RSVPs cannot be accepted due to this constraint. We hope you understand!'
 		},
 		{
-			question: 'What is the dress code for the wedding?',
-			answer:
-				'The dress code for our wedding is semi-formal. We encourage guests to dress in cocktail attire. No jeans or shorts please! Also, prepare for average temperatures in the upper 60s to low 50s.'
-		},
-		{
 			question: 'Can I bring a plus one?',
 			answer:
 				'We kindly request that you refer to your invitation to see if a plus one is extended to you. If no mention of a plus one is indicated, we apologize for any inconvenience.'
 		},
 		{
-			question: 'Will there be vegetarian/vegan meal options?',
+			question: 'Are kids welcome?',
 			answer:
-				'For guests who prefer vegetarian meals, we have made sure to include some delicious options on our menu. We will be serving quesadillas and potato tacos, both of which are sure to satisfy your taste buds. If you have any other dietary restrictions or allergies, please let us know in advance so we can make accommodations for you.'
+				'While we love all your children, this will be an adult only event. The only exception will be the children apart of our wedding party! Please refer to the ‘RSVP’ tab to see if we are able to accommodate your children. We hope that you will see this as an opportunity to let down your hair down and enjoy the party with us!'
+		},
+		{
+			question: 'What is the dress code for the wedding?',
+			answer:
+				'The dress code for our wedding is semi-formal. We encourage guests to dress in cocktail attire. No jeans or shorts please! Also, prepare for average temperatures in the upper 60s to low 50s.'
 		},
 		{
 			question: 'What time does the wedding ceremony start?',
 			answer:
-				'The wedding ceremony will begin promptly at 5:00pm. We encourage guests to arrive at least 30 minutes prior to the start time.'
-		},
-		{
-			question: 'What if I am running late to the ceremony?',
-			answer:
-				'The ceremony will be starting promptly at 5:00 p.m. We recommend guests arrive around 4:30 p.m. so there is time to mingle, grab a drink, and find a seat. If you happen to be late, our coordinator will let you know if they are still allowing guests in or if you will need to wait until the ceremony is over. If that happens, you will still be able to attend the cocktail hour and reception so hang tight and we will see you at the cocktail hour! Thank you for understanding!'
-		},
-		{
-			question: 'Can I take photos during the ceremony?',
-			answer:
-				'To ensure that everyone can enjoy the moment fully, please refrain from taking photos or using your mobile devices during the ceremony. We have hired professional photographers who will capture the beautiful moments for us to share. During the reception, you are welcome and encouraged to take photos! Feel free to use our wedding hashtag, #pattonpending, so we can see all the fun!'
-		},
-		{
-			question: 'Is there a gift registry?',
-			answer:
-				'We appreciate your presence at our wedding, and your well wishes are the greatest gift we could ask for! However, if you wish to honour us with a gift, a cash gift would be very welcome! Your generous contributions will be used to create lasting memories and support our future goals together. If you prefer to give a physical gift, we have a registry set up at <a href="https://www.potterybarn.com/registry/mksskl92cj/registry-list.html" target="_blank" class="underline">Pottery Barn, Williams Sonoma and West Elm here</a>.'
+				'The wedding ceremony will begin promptly at 5:00pm. We recommend guests arrive around 4:30 p.m. so there is time to mingle and find a seat. <br>If you happen to be late, our coordinator will let you know if they are still allowing guests in or if you will need to wait until the ceremony is over. If that happens, you will still be able to attend the cocktail hour and reception so hang tight and we will see you at the cocktail hour! Thank you for understanding!'
 		},
 		{
 			question: 'Will there be parking available at the venue?',
@@ -63,9 +48,14 @@
 				'Unfortunately, we cannot accommodate any overnight parking at the venue. We recommend that guests arrange for a designated driver or ride-sharing service if they plan on drinking at the wedding.'
 		},
 		{
-			question: 'Are kids welcome?',
+			question: 'Can I take photos during the ceremony?',
 			answer:
-				'While we love all your children, this will be an adult only event. The only exception will be the children apart of our wedding party! Please refer to the ‘RSVP’ tab to see if we are able to accommodate your children. We hope that you will see this as an opportunity to let down your hair down and enjoy the party with us!'
+				'To ensure that everyone can enjoy the moment fully, please refrain from taking photos or using your mobile devices during the ceremony. We have hired professional photographers who will capture the beautiful moments for us to share. During the reception, you are welcome and encouraged to take photos! Feel free to use our wedding hashtag, #pattonpending, so we can see all the fun!'
+		},
+		{
+			question: 'Is there a gift registry?',
+			answer:
+				'We appreciate your presence at our wedding, and your well wishes are the greatest gift we could ask for! However, if you wish to honour us with a gift, a cash gift would be very welcome! Your generous contributions will be used to create lasting memories and support our future goals together. If you prefer to give a physical gift, we have a registry set up at <a href="https://www.potterybarn.com/registry/mksskl92cj/registry-list.html" target="_blank" class="underline">Pottery Barn, Williams Sonoma and West Elm here</a>.'
 		}
 	];
 </script>
